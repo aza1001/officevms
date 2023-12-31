ccf1c90e2d848984ce558bf3da2010369acda315
aza1001 <azizah.zainal11@gmail.com>
2023-12-31 17:41:54 +0800

    restart

--- index.js
+++ index.js
@@ -20,14 +20,19 @@ const staffCollection = 'staff';
 const securityCollection = 'security';
 const appointmentCollection = 'appointments';
 
+let staffDB, securityDB, appointmentDB;
+
 // MongoDB connection
 mongodb.MongoClient.connect(mongoURL, { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db(dbName);
-    const staffDB = db.collection(staffCollection);
-    const securityDB = db.collection(securityCollection);
-    const appointmentDB = db.collection(appointmentCollection);
+    staffDB = db.collection(staffCollection);
+    securityDB = db.collection(securityCollection);
+    appointmentDB = db.collection(appointmentCollection);
   })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 // Middleware for authentication and authorization
 const authenticateToken = (req, res, next) => {

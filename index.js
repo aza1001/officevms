a2fd5ee3385399e585eba9e2db6b78e43aba7cda
aza1001 <azizah.zainal11@gmail.com>
2023-12-31 16:05:35 +0800

    add mongo

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const mongodb = require('mongodb')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
+// MongoDB connection URL
+const mongoURL =
+  'mongodb+srv://aza:mongoaza@officevms.tilw1nt.mongodb.net/?retryWrites=true&w=majority';
+
+
 app.get('/', (req, res) => {
    res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)

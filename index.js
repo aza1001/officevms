0314b3748729ef03b81267f4848b3bba5a507497
aza1001 <azizah.zainal11@gmail.com>
2023-12-31 16:25:15 +0800

    setting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000;
+const secretKey = 'officeapt';
 
 app.use(express.json())
 
@@ -12,10 +13,42 @@ app.use(express.json())
 const mongoURL =
   'mongodb+srv://aza:mongoaza@officevms.tilw1nt.mongodb.net/?retryWrites=true&w=majority';
 
+const dbName = 'office appointment';
+const staffCollection = 'staff';
+const securityCollection = 'security';
+const appointmentCollection = 'appointments';
+
+// MongoDB connection
+mongodb.MongoClient.connect(mongoURL, { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db(dbName);
+    const staffDB = db.collection(staffCollection);
+    const securityDB = db.collection(securityCollection);
+    const appointmentDB = db.collection(appointmentCollection);
+  })
+
+// Middleware for authentication and authorization
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).send('Missing token');
+  }
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      return res.status(403).send('Invalid or expired token');
+    }
+    req.user = user;
+    next();
+  });
+};
 
 app.get('/', (req, res) => {
    res.send('Hello World!')
 })
+
 
 
 app.listen(port, () => {
